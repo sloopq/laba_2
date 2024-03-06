6779d3bccfa245c72cee0990b3f20cc4d026f394
sloopq <vovanchik058905@gmail.com>
2024-03-06 10:53:32 +0200

    add ex2

--- code/index.php
+++ code/index.php
@@ -10,3 +10,24 @@ $order .= ' and fries';
   
  // Don't change the line below
  echo "\nYour order is: $very_bad_unclear_name.";
+ echo "\n";
+
+
+ $a = 15;
+ echo $a;
+ echo "\n";
+ 
+ $b = 16.3;
+ echo $b;
+ echo "\n";
+
+ echo 16 - 4;
+ echo "\n";
+
+
+ 
+ $last_month = 1187.23;
+ $this_month = 1089.98;
+
+ echo "Разница " . ($last_month - $this_month);
+ echo "\n";
